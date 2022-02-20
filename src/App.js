38f615cc51707d6cd7fc0c2e34c674960c6ae417
daisy77125 <32116413+daisy77125@users.noreply.github.com>
2022-02-19 20:19:27 -0500

    removed some files generated by create-react-app; installed utility packages & json-server as dev dependency; established layout with header, footer and content with route.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Orders from "./components/Orders";
+import Create from "./components/Create";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container d-flex flex-column min-vh-100">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Orders />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-light text-center">
+      <div className="container">
+        <span className="text-muted">Copyright &copy; 2022 Domain Name</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
