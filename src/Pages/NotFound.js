26660e007e373930ed66304922bc1f3bb4f9268f
daisy77125 <32116413+daisy77125@users.noreply.github.com>
2022-02-20 23:41:48 -0500

    formatted NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="text-center">
+      <span className="px-2">Page Not Found</span>
+      <i className="bi bi-emoji-frown"></i>
+    </div>
+  );
 };
 
 export default NotFound;
